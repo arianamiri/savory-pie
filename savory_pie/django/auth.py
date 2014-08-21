51961d19cddbb27c22162429492518c10e335a4d
arianamiri <aamiri@ruelala.com>
2014-08-21 17:43:23 -0400

    add uri_auth_adapter

--- savory_pie/django/auth.py
+++ savory_pie/django/auth.py
@@ -1,3 +1,39 @@
+from savory_pie.fields import URIResourceField, URIListResourceField
+from savory_pie.django.fields import RelatedManagerField
+
+
+def uri_auth_adapter(field, ctx, source_dict, target_obj):
+    """
+    Authorization adapter for use in fields representing a 1 to many relationship.  Is used when you want to prevent
+    unauthorized users from changing the associations of different models.
+    """
+    name = field._compute_property(ctx)
+    source_field = source_dict[name]
+    target_field = getattr(target_obj, field.name)
+    source = None
+    target = None
+
+    if source_field and target_field:
+        if isinstance(field, RelatedManagerField):
+            source = [source_field_item['resourceUri'] for source_field_item in source_field]
+            target = [ctx.build_resource_uri(field._resource_class(target_item)) for target_item in target_field.all()]
+            source.sort()
+            target.sort()
+        elif isinstance(field, URIResourceField):
+            source = source_field
+            target = ctx.build_resource_uri(field._resource_class(target_field))
+        elif isinstance(field, URIListResourceField):
+            source = source_field
+            target = [ctx.build_resource_uri(field._resource_class(target_item)) for target_item in target_field.all()]
+            source.sort()
+            target.sort()
+        else:
+            raise TypeError('uri_auth_adapter can only be used with fields of type URIResourceField,' +
+                            ' URIListResourceField or RelatedManagerField')
+
+    return name, source, target
+
+
 class DjangoUserPermissionValidator(object):
     """
     Permissions Validator is used to tie into an authorization.  Is used in conjunction with the authorization decorator
--- savory_pie/tests/django/test_auth.py
+++ savory_pie/tests/django/test_auth.py
@@ -1,6 +1,73 @@
 import unittest
 from mock import Mock
-from savory_pie.django.auth import DjangoUserPermissionValidator
+from savory_pie.fields import URIResourceField, URIListResourceField
+from savory_pie.django.auth import DjangoUserPermissionValidator, uri_auth_adapter
+from savory_pie.django.fields import RelatedManagerField
+
+
+class URIAuthorizationAdapterTestCase(unittest.TestCase):
+
+    def _passthrough_method(self, val):
+        return val
+
+    def test_uri_auth_adapter_with_related_manager_field(self):
+        field = Mock(spec=RelatedManagerField, name='field')
+        field.name = 'fieldName'
+        field._resource_class.side_effect = self._passthrough_method
+        field._compute_property.return_value = 'source_name'
+
+        ctx = Mock(spec=['build_resource_uri'])
+        ctx.build_resource_uri.side_effect = self._passthrough_method
+
+        target_obj = Mock(spec=['fieldName'])
+        target_obj.fieldName = Mock(spec=['all'])
+        target_obj.fieldName.all.return_value = ['uri1', 'uri3', 'uri2']
+
+        source_dict = {'source_name': [{'resourceUri': 'uri2'}, {'resourceUri': 'uri1'}]}
+
+        name, source, target = uri_auth_adapter(field, ctx, source_dict, target_obj)
+        self.assertEqual(name, 'source_name')
+        self.assertEqual(source, ['uri1', 'uri2'])
+        self.assertEqual(target, ['uri1', 'uri2', 'uri3'])
+
+    def test_uri_auth_adapter_with_uri_resource_field(self):
+        field = Mock(spec=URIResourceField)
+        field.name = 'fieldName'
+        field._resource_class.side_effect = self._passthrough_method
+        field._compute_property.return_value = 'source_name'
+
+        ctx = Mock(spec=['build_resource_uri'])
+        ctx.build_resource_uri.side_effect = self._passthrough_method
+
+        target_obj = Mock(spec=['fieldName'])
+        target_obj.fieldName = 'uri2'
+
+        source_dict = {'source_name': 'uri1'}
+
+        name, source, target = uri_auth_adapter(field, ctx, source_dict, target_obj)
+        self.assertEqual(name, 'source_name')
+        self.assertEqual(source, 'uri1')
+        self.assertEqual(target, 'uri2')
+
+    def test_uri_auth_adapter_with_uri_list_resource_field(self):
+        field = Mock(spec=URIListResourceField)
+        field.name = 'fieldName'
+        field._resource_class.side_effect = self._passthrough_method
+        field._compute_property.return_value = 'source_name'
+
+        ctx = Mock(spec=['build_resource_uri'])
+        ctx.build_resource_uri.side_effect = self._passthrough_method
+
+        target_obj = Mock(spec=['fieldName'])
+        target_obj.fieldName = Mock(spec=['all'])
+        target_obj.fieldName.all.return_value = ['uri3', 'uri1', 'uri2']
+
+        source_dict = {'source_name': ['uri2', 'uri1']}
+
+        name, source, target = uri_auth_adapter(field, ctx, source_dict, target_obj)
+        self.assertEqual(name, 'source_name')
+        self.assertEqual(source, ['uri1', 'uri2'])
+        self.assertEqual(target, ['uri1', 'uri2', 'uri3'])
 
 
 class DjangoUserPermissionValidatorTestCase(unittest.TestCase):
